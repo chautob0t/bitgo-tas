d822dc7e6a8579834ba55bc7fb37d902405ed641
vaibhav-aj <vaibhav.chauhan@olxpeople.com>
2021-06-28 17:10:10 +0530

    Add helper class to find all ancestors for a given transaction

---
+++ ancestry_helper.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+
+class DFS:
+    def __init__(self, nodes, ancestors):
+        self.one_level_ancestors = ancestors
+        self.adjacency_list = defaultdict(set)
+        self.all_ancestors = defaultdict(set)
+        self.vertices = nodes
+
+    def prepare_adjacency_list(self):
+        for vertex in self.vertices:
+            parents = self.one_level_ancestors[vertex]
+            for parent in parents:
+                self.adjacency_list[parent].add(vertex)
+
+    def __find_all_ancestors(self, vertex, parents=[]):
+        # Update parents of the vertex we are at
+        self.all_ancestors[vertex].add(parents)
+        # Add current vertex to the parents
+        parents.append(vertex)
+        children = self.adjacency_list[vertex]
+        for child in children:
+            # Recursively call DFs for all the children
+            self.find_all_ancestors(child, parents)
+        # As the recursion folds, remove the last parent that we added
+        parents.pop()
+
+    def find_all_ancestors(self):
+        for vertex in self.vertices:
+            # Find all ancestors for each vertex in graph
+            self.__find_all_ancestors(vertex, [])
